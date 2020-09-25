0fd36890ab43a31cfc944cf2bd0f2ae135cf7cd7
king dave <bigthinzcount@gmail.com>
2020-09-25 12:23:34 +0100

    modelling location

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,7 +19,7 @@ mongoose.connect(DB,{
 	console.log("database connected")
 })
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'))
 			
 
 
