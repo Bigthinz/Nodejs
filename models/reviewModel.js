ea6706a38108f76c06245f89da79ee6367d13622
king dave <bigthinzcount@gmail.com>
2020-09-30 00:19:02 +0100

    calculating average rating on tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModel')
 
 
 const reviewSchema = new mongoose.Schema({
@@ -54,6 +55,36 @@ reviewSchema.pre(/^find/, function(next){
 	next()
 })
 
+
+//Aggregating or calculating reviews to displays average reviews
+reviewSchema.statics.calcAverageRatings = async function(tourId){
+	const stats = await this.aggregate([
+	{
+		$match: {tour: tourId}
+	},
+	{
+		$group: {
+			_id: '$tour',
+			nRating: {$sum: 1},
+			avgRating: {$avg: '$rating'}
+		}
+	}
+
+	])
+console.log(stats)
+
+	await Tour.findByIdAndUpdate(tourId,{
+		ratingsQuantity: stats[0].nRating,
+		ratingsAverage: stas[0].avgRating
+	})
+
+}
+
+
+reviewSchema.post('save', function(){
+	//this points to the current review
+	this.constructor.calcAverageRatings(this.tour)
+})
 
 
 const Review = mongoose.model('Review', reviewSchema)
